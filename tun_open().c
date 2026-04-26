21bdc4b8fb2653d88396d0ea62be2c5b50da20f9
GabrielVerissimoL <gabriel14703flg@gmail.com>
2026-04-25 21:30:57 -0300

    tun_open().c
    Início do estudo sobre TUN/TAN no linux para fazer um firewall com isso no futuro

---
+++ tun_open().c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/ioctl.h>
+#include <string.h>
+
+int tun_open() {
+    struct ifreq ifr;
+    int fd, err;
+    char *clonedev = "/dev/net/tun"; // Endereço do arquivo TUN no linux
+    
+    //Abrindo o '/dev/net/tun'
+    if( (fd = open(clonedev, O_RDWR)) < 0 ) { // O_RDWR é uma constante que significa Open for Read and Write
+        perror("Opening /dev/net/tun");
+        return fd;
+    }    
+
+    //Limpa a struct ifreq
+    memset(&ifr, 0, sizeof(ifr));
+
+    //Setando as flags necessárias para continuar ( | combina duas flags em uma só)
+    ifr.ifr_flags = IFF_TUN | IFF_NO_PI;
+
+    if( (err = ioctl(fd, TUNSETIFF, (void *)&ifr)) < 0 ) {
+        perror("ioctl(TUNSETIFF)");
+        close(fd);
+        return err;
+    }
+
+
+    
+    
+    
+    
+    
+    return fd;
+
+}
